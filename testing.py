248f2078566b3fde31051a1d7057fb9811bb1316
phc-11 <114267789+phc-11@users.noreply.github.com>
2023-07-26 15:34:14 -0500

    Updating testing.py
    Added error catching to testing.py

--- testing.py
+++ testing.py
@@ -11,19 +11,27 @@ from selenium.webdriver.support import expected_conditions
 import time
 
 #Establish the path to the Chrome Driver
-path = '/Users/preas/Downloads/chromedriver_win32' 
+path = '/Users/preas/Downloads/chromedriver_win32/chromedriver.exe' 
 
+#Establish ChromeOptions which will allow us to control extra options for our browser driver
+options = webdriver.ChromeOptions()
+
+#ChromeOption to set the window to start maximized
+options.add_argument("--start-maximized")
+
+#ChromeOption to limit the console bound errors
+options.add_experimental_option('excludeSwitches',['enable-logging'])
+
+#Try & Exception to handle error logging through the CLI
+try:
 #Enitialize the instance of our browser driver
-driver = webdriver.Chrome()
+    driver = webdriver.Chrome(options=options)
 
 #Tell the driver to wait for the page to initizalize
-driver.implicitly_wait(200)
+    driver.implicitly_wait(200)
 
 #Give the driver the webpage we are going to scrape
-driver.get('https://www.pokemon.com/us/pokedex')
-
-#Set the browser window to maximum size
-#driver.maximize_window()
+    driver.get('https://www.pokemon.com/us/pokedex')
 
 #Set what element we are going to scrape for
 #element = 'h5'
@@ -32,18 +40,33 @@ driver.get('https://www.pokemon.com/us/pokedex')
 #element = driver.find_elements(By.TAG_NAME,'h5')
 
 #Search for the element by CLASS WORKS
-element = driver.find_elements(By.CLASS_NAME,'id')
+    element = driver.find_elements(By.CLASS_NAME,'id')
+
+#Except to print an error to the CLI and exit gracefully
+except:
+    print("An error has occurred.")
+    print("Please try again.")
+    driver.quit()
+    exit(0)
+
 
 #Holds the program so the user can load all elements on the page
 input("Press ENTER when ready")
 
-#Error checking
-print("This is the print: \n")
-print("This is element_test: ")
 
-#For loop to print the elements from out element list
-for e in element:
-    print(e.text)
+#Try & Except to handle manual browser closing.
+try:
+
+    #For loop to print the elements from out element list
+    for e in element:
+        print(e.text)
+
+#Except to print an error message, quit the driver, and exit the program gracefully
+except: 
+    print("Please do not close the browser.")
+    print("Please try again.")
+    driver.quit()
+    exit(0)
 
 
 #.quit() dumps contents so needs to executed last
