00821bf26a81d2f312b397799590681bffff8f14
phc-11 <114267789+phc-11@users.noreply.github.com>
2023-07-27 15:59:03 -0500

    Moved test to scraper
    Moved the logic of test.py to scraper.py (model) to port to functions/GUI

--- scraper.py
+++ scraper.py
@@ -1,22 +1,76 @@
-import os
-import pip._vendor.requests 
-import requests
-from bs4 import BeautifulSoup
-import scraperControl
-import scraperView
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver import *
 
-URL = input("Enter the webpage you would like to scrape:").rstrip()
-page = requests.get(URL)
+#Establish the path to the Chrome Driver
+#path = '/Users/preas/Downloads/chromedriver_win32/chromedriver.exe' 
 
-soup = BeautifulSoup(page.content,"html.parser")
+#Establish ChromeOptions which will allow us to control extra options for our browser driver
+options = webdriver.ChromeOptions()
 
-elementType = input("What HTML element would you like to scrape for (Class, Div, ID, etc.)")
+#ChromeOption to set the window to start maximized
+options.add_argument("--start-maximized")
 
-elementName = input("What is the name of the element(s) you would like to scrape?")
+#ChromeOption to limit the console bound errors
+options.add_experimental_option('excludeSwitches',['enable-logging'])
+
+#Try & Exception to handle error logging through the CLI
+try:
+#Enitialize the instance of our browser driver
+    driver = webdriver.Chrome(options=options)
+
+#Tell the driver to wait for the page to initizalize
+    driver.implicitly_wait(200)
+
+#Give the driver the webpage we are going to scrape
+    driver.get('https://www.pokemon.com/us/pokedex')
+
+#Set what element we are going to scrape for
+#element = 'h5'
+
+#Search for the element by TAG WORKS
+#element = driver.find_elements(By.TAG_NAME,'h5')
+
+#Search for the element by CLASS WORKS
+    element = driver.find_elements(By.CLASS_NAME,'id')
+
+#Except to print an error to the CLI and exit gracefully
+except:
+    print("An error has occurred.")
+    print("Please try again.")
+    driver.quit()
+    exit(0)
 
 
-if (elementType == "Class"):
-    results = soup.find(class_=elementName)
+#Holds the program so the user can load all elements on the page
+input("Press ENTER when ready")
 
 
-print(results.text)
+#Try & Except to handle manual browser closing.
+try:
+
+    #For loop to print the elements from out element list
+    for e in element:
+        print(e.text)
+
+#Except to print an error message, quit the driver, and exit the program gracefully
+except: 
+    print("Please do not close the browser.")
+    print("Please try again.")
+    driver.quit()
+    exit(0)
+
+
+#.quit() dumps contents so needs to executed last
+driver.quit()
+
+
+
+
+
+
+#Need to make into a function, show the user the webpage before the scrape; hopefully that will allow the user to modify "see more.." buttons to get a full scrape,
+#Show the user an outline of what is being scraped from the webpage, make sure this can be used with a string being fed; so we can edit attributes easier
+
+
+#We must give the user time to load the entirety of the webpage, this will allow the user to scrape all elements
--- testing.py
+++ testing.py
@@ -1,14 +1,6 @@
-import requests
-import urllib.request
-import os
-import webbrowser
-import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver import *
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions
-import time
 
 #Establish the path to the Chrome Driver
 path = '/Users/preas/Downloads/chromedriver_win32/chromedriver.exe' 
